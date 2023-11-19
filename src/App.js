a3e111e44b65f0e090281ac186cff3718d23b6ff
bhavyaa-anand2003 <bhavyaanand982003@gmail.com>
2023-11-20 02:20:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import Header from './Components/Header'
-import Body   from './Components/Body'
-import Footer from './Components/Footer'
+import Navigation from './Components/Navigation'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import TodoList from './Components/TodoList'
+
+import {BrowserRouter as Router ,Routes, Route} from
+ 'react-router-dom'
 const App = () => {
   return (
-    <div><Header></Header>
-    <Body></Body>
-    <Footer></Footer>
+    <div>
+      <Router>
+        <div>
+            <Navigation/>
+        </div>
+    
+    
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/todolist'element={<TodoList />} />
+    </Routes>
+    </Router>
     </div>
   )
 }
-
 export default App
---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+     <phone number>+9980530259</phone><br/>
+     <email>Bhavyaanand@gmail.com</email><br/>
+     <socialmedia>github:1SV21CS012@shrideviengineering.org</socialmedia><br/> 
+    </div>
+  )
+}
+
+export default Contact
